20dcb0edbcfccbb1ba9a3c54e4bc41d3e9d24f29
develper21 <githubnarvin@gmail.com>
2025-11-05 20:03:07 +0530

    in taskRoutes.js file i was update get,put,delete routes and add constValidatorMiddelware.js file

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -11,6 +11,7 @@ const {
   updateTaskStatus,
   updateTaskChecklist,
 } = require("../controllers/taskController");
+const { validateTask, validateObjectId } = require("../middlewares/validationMiddleware");
 
 const router = express.Router();
 
@@ -18,11 +19,11 @@ const router = express.Router();
 router.get("/dashboard-data", protect, getDashboardData);
 router.get("/user-dashboard-data", protect, getUserDashboardData);
 router.get("/", protect, getTasks); // get all tasks (admin: all, user: assigned)
-router.get("/:id", protect, getTaskById); // get specific tasks
-router.post("/", protect, adminOnly, createTask); // create a task (admin only)
-router.put("/:id", protect, updateTask); // update task detail
-router.delete("/:id", protect, adminOnly, deleteTask); // delete task (admin only)
-router.put("/:id/status", protect, updateTaskStatus); // update task status
-router.put("/:id/todo", protect, updateTaskChecklist); // update task checklist
+router.get("/:id", protect, validateObjectId("id"), getTaskById); // get specific tasks
+router.post("/", protect, adminOnly, validateTask, createTask); // create a task (admin only)
+router.put("/:id", protect, validateObjectId("id"), validateTask, updateTask); // update task detail
+router.delete("/:id", protect, adminOnly, validateObjectId("id"), deleteTask); // delete task (admin only)
+router.put("/:id/status", protect, validateObjectId("id"), updateTaskStatus); // update task status
+router.put("/:id/todo", protect, validateObjectId("id"), updateTaskChecklist); // update task checklist
 
 module.exports = router;
